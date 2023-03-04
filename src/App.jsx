f096251154bb176ec7e37f6d3079daf9c09a4f7e
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2023-03-04 19:07:31 +0530

    added file folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>hi</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
